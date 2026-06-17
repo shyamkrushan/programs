ddbc86d3022ca310145d99e394b15c03c3cc728e
Shyam Krushan S <sankarnarayanan916_bec27@mepcoeng.ac.in>
2026-06-17 15:51:11 +0530

    fibonaccinumber.py

---
+++ fibonaccinumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        a, b, i = 0, 1, 1
+        while i < n:
+            a, b = b, a + b
+            i += 1
+        return b
